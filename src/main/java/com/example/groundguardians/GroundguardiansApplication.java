1ff34447ea3d9f42ce6feeed3ab116d0bbcd541c
suminnnnn <tlsssm1212@ajou.ac.kr>
2024-01-25 23:45:10 +0900

    원격 저장소 연결

---
+++ src/main/java/com/example/groundguardians/GroundguardiansApplication.java
@@ -0,0 +1,13 @@
+package com.example.groundguardians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundguardiansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroundguardiansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/groundguardians/GroundguardiansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groundguardians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundguardiansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
