1318be2cc84fbb4b82b112724a4e3c9b7f5c9592
suminnnnn <tlsssm1212@ajou.ac.kr>
2024-02-04 03:35:39 +0900

    CONFIG: Swagger JWT Token 관련 설정 추가

--- src/main/java/com/example/groundguardians/config/SwaggerConfig.java
+++ src/main/java/com/example/groundguardians/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.groundguardians.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +15,18 @@ public class SwaggerConfig {
         Info info = new Info()
                 .title("Ground Guardians API Document")
                 .description("One Peace 팀의 플랫폼 Ground Guardians의 API 명세서입니다.");
+
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .info(info)
+                .addSecurityItem(securityRequirement);
     }
 }
